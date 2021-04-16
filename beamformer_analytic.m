c961e461c7bf07afdc9a0e8363eeebef19bdfd84
joller134 <jasper-zw@hotmail.com>
2021-04-16 17:05:16 +0200

    fixed corralation

--- beamformer_analytic.m
+++ beamformer_analytic.m
@@ -7,8 +7,9 @@ for nmbSource = 1:length(sources)
 delayArray = zeros(1,length(sunFlowerArray))
 for i = 1:length(sunFlowerArray)
    x = xcorr(STSS,readOut(i+(nmbSource-1)*length(sunFlowerArray),:));
-   %steps = find(x==max(x));
-   steps = find(readOut(i+(nmbSource-1)*length(sunFlowerArray),:)>10,1);
+   steps = find(x>10^4);
+   steps = -y(steps(end));
+   %steps = find(readOut(i+(nmbSource-1)*length(sunFlowerArray),:)>10,1);
    %steps = find(x>10^6,1)
    delayArray(i) = steps*t_array(2);
 end
@@ -23,9 +24,9 @@ for i = 2:length(sunFlowerArray)
     distanceVector = sunFlowerArray(i,:)-sunFlowerArray(1,:);
     distance = sqrt(sum(distanceVector.^2))
     distanceWave = (delayArray(i)*medium_speed); 
-    angle = asind(distanceWave/distance)
+    angleVector = asind(distanceWave/distance)
     
-    waveVector(i,:) = distanceVector*sind(angle)
+    waveVector(i,:) = distanceVector*sind(angleVector)
 end
 
 
@@ -43,3 +44,28 @@ subplot(3,1,3)
 scatter(sunFlowerArray(:,1),sunFlowerArray(:,2), 50, t(:,3), 'filled')
 axis image
 title("Third source")
+
+Y = fft(readOut(1,:));
+Fs = 1/(t_array(2));
+L = length(Y);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+
+figure
+f = Fs*(0:(L/2))/L;
+plot(f,P1) 
+title('Single-Sided Amplitude Spectrum of X(t)')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
+
+figure
+subplot(2,1,1)
+x = xcorr(STSS,readOut(1,:))
+plot(y,x)
+xlim([-6e3 0])
+subplot(2,1,2)
+plot(STSS)
+hold all
+plot(readOut(1,:))
+xlim([0 6e3])
